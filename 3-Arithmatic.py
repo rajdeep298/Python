0481254cfc0c6512644dc0189ae2827bdde1c569
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-10-11 15:03:59 +0530

    Some mild changes

--- 3-Arithmatic.py
+++ 3-Arithmatic.py
@@ -8,3 +8,12 @@ mult = val1 * val2
 print("Multiplication:", mult)
 div = val1 / val2
 print("Division:", div)
+
+def GCD(a,b):
+    if b==0:
+        return a
+    else:
+        return GCD(b,int(a%b))
+
+gcd= GCD(val1,val2)
+print("GCD:",gcd)
