3dbbc4c3aeee98db7d048f7621bd9755d5de8158
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-12-10 19:38:06 +0530

    Some mild changes

--- Lists.py
+++ Lists.py
@@ -1,77 +1,77 @@
 # Nested Lists
-List1=[[1,2,3],[4,5,6],[7,8,9]]
+List1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(List1)
 
 # Accessing elements in a nested list
-print(List1[0])#first sub-list
-print(List1[0][0])#first element of first sub-list
+print(List1[0])  # first sub-list
+print(List1[0][0])  # first element of first sub-list
 
-#Slice a nested list
-print(List1[0:2])#first two sub-lists
-print(List1[0][0:2])#first two elements of first sub-list
+# Slice a nested list
+print(List1[0:2])  # first two sub-lists #Output: [[1, 2, 3], [4, 5, 6]]
+print(List1[0][0:2])  # first two elements of first sub-list #Output: [1, 2]
 
-#List comprehension
-List2=[i for i in range(10)]
-print(List2)
+# List comprehension
+List2 = [i for i in range(10)]
+print(List2)  # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-List3=[i for i in range(10) if i%2==0]
-print(List3)
+List3 = [i for i in range(10) if i % 2 == 0]
+print(List3)  # Output: [0, 2, 4, 6, 8]
 
-List4=[i for i in range(10) if i%2!=0]
-print(List4)
+List4 = [i for i in range(10) if i % 2 != 0]
+print(List4)  # Output: [1, 3, 5, 7, 9]
 
-#List comprehension with nested list
-List5=[[i for i in range(3)] for j in range(3)]
-print(List5)
+# List comprehension with nested list
+List5 = [[i for i in range(3)] for j in range(3)]
+print(List5)  # Output: [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
 
-List6=[[i for i in range(3)] for j in range(3) if j%2==0]
-print(List6)
+List6 = [[i for i in range(3)] for j in range(3) if j % 2 == 0]
+print(List6)  # Output: [[0, 1, 2], [0, 1, 2]] prints only sub-lists when j is even
 
-List7=[[i for i in range(3)] for j in range(3) if j%2!=0]
-print(List7)
+List7 = [[i for i in range(3)] for j in range(3) if j % 2 != 0]
+print(List7)  # Output: [[0, 1, 2], [0, 1, 2]] prints only sub-lists when j is odd
 
-#Iterating through a nested list
-for i in List1:#iterate through each sub-list
-    print(i,end='\t')
+# Iterating through a nested list
+for i in List1:  # iterate through each sub-list
+    print(i, end='\t')
 print()
 
-for i in List1:#iterate through each element
+for i in List1:  # iterate through each element
     for j in i:
-        print(j,end='\t')
+        print(j, end='\t')
     print()
 
-#Append
+# Append
 
-#apppend a list
-List2.append(10)#append 10 at the end of the list
-print(List2)
-#append a nested list
-List1.append([10,11,12])#append a list at the end of the list
-print(List1)
+# apppend a list
+List2.append(10)  # append 10 at the end of the list
+print(List2)  # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+# append a nested list
+List1.append([10, 11, 12])  # append a list at the end of the list
+print(List1)  # Output: [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10,11,12]]
 
-#Extend
-List2.extend(List3)#extend the list by appending all the items from the iterable
-print(List2)
-List3.extend("Hello")#extend the list by appending all the items from the iterable
-print(List3)
+# Extend
+List2.extend(List3)  # extend the list by appending all the items from the iterable
+print(List2)  # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 0, 2, 4, 6, 8]
+List3.extend("Hello")  # extend the list by appending all the items from the iterable
+print(List3)  # Output: [0, 2, 4, 6, 8, 'H', 'e', 'l', 'l', 'o'] reading hello by characters
 
-#Insert
-List2.insert(0,0)#insert an item at a given position
-print(List2)
+# Insert
+List2.insert(0, 0)  # insert an item at a given position
+print(List2)  # Output: [0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 0, 2, 4, 6, 8]
 
-#Remove
-List2.remove(0)#remove the first item from the list whose value is 0
-print(List2)
+# Remove
+List2.remove(0)  # remove the first item from the list whose value is 0
+print(List2)  # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 0, 2, 4, 6, 8]
 
-#Pop
-List2.pop()#remove the last item from the list
-print(List2)
+# Pop
+List2.pop()  # remove the last item from the list
+print(List2)  # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 0, 2, 4, 6]
 
-#Clear
-List2.clear()#remove all items from the list
-print(List2)
+# Clear
+List2.clear()  # remove all items from the list
+print(List2)  # Output: []
 
-#Split
-string1="Hello World"
-List8=string1.split(" ")#split a string into a list
-print(List8)
+# Split
+string1 = "Hello World"
+List8 = string1.split(" ")  # split a string into a list
+print(List8)  # Output: ['Hello', 'World']
--- Strings.py
+++ Strings.py
@@ -1,12 +1,12 @@
-string1='Rajdeep'
-string2="Rajdeep" #both are same
-print(string1)
-print(string2)
+string1 = 'Rajdeep'
+string2 = "Rajdeep"  # both are same
+print(string1)  # Output: Rajdeep
+print(string2)  # Output: Rajdeep
 
 # Multiline string
-string3='''Hello
+string3 = '''Hello
             World'''
-print(string3)
+print(string3)  # Output: Hello World
 
 # Accessing characters in a string
 print(string1[0])
@@ -35,55 +35,54 @@ print(string1[:6])
 print(string1[0:])
 print(string1[:])
 
-#Slicing with step
-print(string1[0:7:2])
-print(string1[::-1]) #reverse string
+# Slicing with step
+print(string1[0:7:2])  # Output: Rjdp
+print(string1[::-1])  # reverse string #Output: peedjaR
 
 # String concatenation
-print(string1+string2)
+print(string1 + string2)  # Output: RajdeepRajdeep
 
 # String repetition
-print(string1*3)
+print(string1 * 3)  # Output: RajdeepRajdeepRajdeep
 
 # String length
-print(len(string1))
+print(len(string1))  # Output: 7
 
 # String membership
-print('Raj' in string1)
+print('Raj' in string1)  # Output: True
 
 # Raw string
-RawString=r'Hello\nWorld' #it treats backslashes as literal character
+RawString = r'Hello\nWorld'  # it treats backslashes as literal character
 print(RawString)
 
 # String formatting using format operator
-print("My name is {}".format(string1))
+print("My name is {}".format(string1))  # Output: My name is Rajdeep
 
 # String formatting using % operator
-print("My name is %s" % string1)
+print("My name is %s" % string1)  # Output: My name is Rajdeep
 
 # String formatting using f-strings
-print(f"My name is {string1}")
+print(f"My name is {string1}")  # Output: My name is Rajdeep
 
 # Count method
-print(string1.count('e'))# returns the number of occurrences of substring in string
+print(string1.count('e'))  # returns the number of occurrences of substring in string
 
 # Find method
-print(string1.find('e'))# returns the index of first occurrence of substring in string
+print(string1.find('e'))  # returns the index of first occurrence of substring in string
 
 # Replace method
-print(string1.replace('e','i'))# returns a copy of string with all occurrences of substring old replaced by new
+print(string1.replace('e', 'i'))  # returns a copy of string with all occurrences of substring old replaced by new
 
 # Split method
-print(string1.split('e'))# returns a list of all the words in the string, using str as the separator
+print(string1.split('e'))  # returns a list of all the words in the string, using str as the separator
 
 # Strip method
-string3='    Hello    '
+string3 = '    Hello    '
 print(string3)
-print(string3.strip())# returns a copy of the string with the leading and trailing characters removed
+print(string3.strip())  # returns a copy of the string with the leading and trailing characters removed
 
 # Upper method
-print(string1.upper())# returns a copy of the string converted to uppercase
+print(string1.upper())  # returns a copy of the string converted to uppercase
 
 # Lower method
-print(string1.lower())# returns a copy of the string converted to lowercase
-
+print(string1.lower())  # returns a copy of the string converted to lowercase
