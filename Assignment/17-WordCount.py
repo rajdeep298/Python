b75568e68031d999d12a1b6248fca1d62da8f39d
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-12-29 23:10:52 +0530

    some mild changes

---
+++ Assignment/17-WordCount.py
@@ -0,0 +1,21 @@
+import sys
+
+fp = open(sys.argv[1], 'r')
+
+lines = fp.readlines()
+
+txt = []
+
+for i in lines:
+    i = i.strip().lower().replace(',', '').replace('.', '').replace('?', '').replace('!', '').replace(':', '').replace(
+        ';', '')
+    txt.append(i.split(' '))
+
+word_count = 0
+
+str = input("Enter the word to be searched: ")
+
+for i in txt:
+    word_count += i.count(str.lower())
+
+print("The word", str, "occurs", word_count, "times in the file", sys.argv[1])
