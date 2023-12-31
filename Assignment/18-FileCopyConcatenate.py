d40fb7ee43055179fa533d57eef13f74dbd6d4fa
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-01-01 01:00:44 +0530

    some mild changes

--- Assignment/18-FileCopyConcatenate.py
+++ Assignment/18-FileCopyConcatenate.py
@@ -1,12 +1,12 @@
-#copying content of one file to another file
+# copying content of one file to another file
 import sys
 
-n=int(input("Enter 1 for write and 2 for append: "))
+n = int(input("Enter 1 for write and 2 for append: "))
 
-ch='y'
+ch = 'y'
 
-while ch=='y':
-    if n==1:
+while ch == 'y':
+    if n == 1:
         fp1 = open(sys.argv[1], 'r')
         fp2 = open(sys.argv[2], 'w')
 
@@ -15,8 +15,8 @@ while ch=='y':
 
         fp1.close()
         fp2.close()
-    if n==2:
-        #concatenating two files
+    if n == 2:
+        # concatenating two files
         fp1 = open(sys.argv[1], 'r')
         fp2 = open(sys.argv[2], 'r')
         fp3 = open(sys.argv[3], 'a')
@@ -27,8 +27,8 @@ while ch=='y':
         for line in fp2:
             fp3.write(line)
 
-    ch=input("Do you want to continue? y/n: ")
-    ch=ch.lower()
+    ch = input("Do you want to continue? y/n: ")
+    ch = ch.lower()
     fp3.close()
 
 fp1.close()
