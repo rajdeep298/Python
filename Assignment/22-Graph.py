11c50a3b3c6516b04525a0e83a0ecc8fa3949994
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-01-16 09:10:15 +0530

    some mild changes

--- Assignment/22-Graph.py
+++ Assignment/22-Graph.py
@@ -53,6 +53,58 @@ class Graph:
                 if self.graph[i][j] == 1:
                     print(i, "->", j, end=",")
 
+    def clustering_coefficient(self):
+        coefficients = {}
+        for node in self.graph:
+            neighbors = self.graph[node]
+            if len(neighbors) < 2:
+                coefficients[node] = 0.0
+            else:
+                total_triangles = 0
+                for neighbor1 in neighbors:
+                    for neighbor2 in neighbors:
+                        if neighbor1 != neighbor2 and neighbor1 in self.graph[neighbor2]:
+                            total_triangles += 1
+                coefficients[node] = total_triangles / (len(neighbors) * (len(neighbors) - 1))
+        return coefficients
+
+    def degree_distribution(self):
+        return {node: len(neighbors) for node, neighbors in self.graph.items()}
+
+    def connected_components(self):
+        visited = set()
+        components = []
+
+        for node in self.graph:
+            if node not in visited:
+                component = self._dfs(node, visited)
+                components.append(component)
+
+        return components
+
+    def _dfs(self, start, visited):
+        component = set()
+        stack = [start]
+
+        while stack:
+            current_node = stack.pop()
+            if current_node not in visited:
+                visited.add(current_node)
+                component.add(current_node)
+                stack.extend(neighbor for neighbor in self.graph[current_node] if neighbor not in visited)
+
+        return component
+
+    def connected_components_as_graphs(self):
+        connected_components = self.connected_components()
+        graphs = [Graph() for _ in range(len(connected_components))]
+
+        for idx, component in enumerate(connected_components):
+            for node in component:
+                graphs[idx].graph[node] = self.graph[node]
+
+        return graphs
+
     def enqueue(self, node):
         self.queue[self.rear] = node
         self.rear += 1
@@ -79,3 +131,43 @@ class Graph:
                     self.src[i] = node
 
         print("BFS->", self.queue)
+
+graph=Graph()
+edge_list = [[0, 0, 1, 2, 3, 3, 4, 4, 4, 5, 5],[1, 2, 2, 3, 4, 5, 5, 6, 7, 6, 7]]
+
+#Graph from edge list
+graph.graphFromEdgeList(edge_list)
+print("Graph->",graph.graph)
+
+#Add node
+graph.nodeAdd(8)
+print("Graph->",graph.graph)
+
+#Add edge
+graph.edgeAdd(8,9)
+print("Graph->",graph.graph)
+
+#Remove node
+graph.nodeRemove(8)
+print("Graph->",graph.graph)
+
+#Remove edge
+graph.edgeRemove(8,9)
+
+#Print node edges
+graph.printNodeEdges()
+
+#Degree Distribution
+print("Degree Distribution->",graph.degree_distribution())
+
+#Clustering Coefficient
+print("Clustering Coefficient->",graph.clustering_coefficient())
+
+#Connected Components
+print("Connected Components->",graph.connected_components())
+
+#Connected Components as Graphs
+print("Connected Components as Graphs->",graph.connected_components_as_graphs())
+
+#BFS
+graph.BFS(0)
